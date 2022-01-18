6e371fb97932a307cc12e7531faea44efd9afbaa
alex-l-kong <alkong@ucdavis.edu>
2022-01-18 10:15:17 -0800

    Add settings.py with just MIBI QC vars

---
+++ creed/settings.py
@@ -0,0 +1,14 @@
+# MIBI tiling parameters
+REGION_PARAM_FIELDS = ['region_start_x', 'region_start_y', 'fov_num_x', 'fov_num_y',
+                       'x_fov_size', 'y_fov_size', 'region_rand']
+MICRON_TO_STAGE_X_MULTIPLIER = 0.001001
+MICRON_TO_STAGE_X_OFFSET = 0.3116
+MICRON_TO_STAGE_Y_MULTIPLIER = 0.001018
+MICRON_TO_STAGE_Y_OFFSET = 0.6294
+STAGE_TO_PIXEL_X_MULTIPLIER = 1 / 0.06887
+STAGE_TO_PIXEL_X_OFFSET = 27.79
+STAGE_TO_PIXEL_Y_MULTIPLIER = 1 / -0.06926
+STAGE_TO_PIXEL_Y_OFFSET = -77.40
+
+# mibitracker
+MIBITRACKER_BACKEND = 'https://backend-dot-mibitracker-angelolab.appspot.com'
