43f0999141797b3a11cdba7199fda5c47382574d
alex-l-kong <alkong@ucdavis.edu>
2022-01-19 11:15:08 -0800

    Nuke more fov_list_info

--- creed/tiling_utils.py
+++ creed/tiling_utils.py
@@ -117,20 +117,20 @@ def generate_region_info(region_params):
     return region_params_list
 
 
-def read_tiled_region_inputs(fov_list_info, region_params):
-    """Reads input for tiled regions from user and `fov_list_info`.
+def read_tiled_region_inputs(region_corners, region_params):
+    """Reads input for tiled regions from user and `region_corners`.
 
     Updates all the tiling params inplace. Units used are microns.
 
     Args:
-        fov_list_info (dict):
-            The data containing the FOVs used to define each tiled region
+        region_corners (dict):
+            The data containing the FOVs used to define the upper-left corner of each tiled region
         region_params (dict):
             A `dict` mapping each region-specific parameter to a list of values per FOV
     """
 
     # read in the data for each fov (region_start from region_corners_path, all others from user)
-    for fov in fov_list_info['fovs']:
+    for fov in region_corners['fovs']:
         region_params['region_start_x'].append(fov['centerPointMicrons']['x'])
         region_params['region_start_y'].append(fov['centerPointMicrons']['y'])
 
@@ -203,22 +203,22 @@ def set_tiled_region_params(region_corners_path):
 
     # read in the fov list data
     with open(region_corners_path, 'r') as flf:
-        fov_list_info = json.load(flf)
+        tiled_region_corners = json.load(flf)
 
     # define the parameter dict to return
     tiling_params = {}
 
-    # copy over the metadata values from fov_list_info to tiling_params
-    tiling_params = assign_metadata_vals(fov_list_info, tiling_params, ['fovs'])
+    # copy over the metadata values from tiled_region_corners to tiling_params
+    tiling_params = assign_metadata_vals(tiled_region_corners, tiling_params, ['fovs'])
 
     # define the region_params dict
     region_params = {rpf: [] for rpf in settings.REGION_PARAM_FIELDS}
 
     # prompt the user for params associated with each tiled region
-    read_tiled_region_inputs(fov_list_info, region_params)
+    read_tiled_region_inputs(tiled_region_corners, region_params)
 
     # need to copy fov metadata over, needed for generate_fov_list
-    tiling_params['fovs'] = copy.deepcopy(fov_list_info['fovs'])
+    tiling_params['fovs'] = copy.deepcopy(tiled_region_corners['fovs'])
 
     # store the read in parameters in the region_params key
     tiling_params['region_params'] = generate_region_info(region_params)
@@ -420,16 +420,16 @@ def generate_tma_fov_list(tma_corners_path, num_fov_x, num_fov_y):
 
     # read in tma_corners_path
     with open(tma_corners_path, 'r') as flf:
-        fov_list_info = json.load(flf)
+        tma_corners = json.load(flf)
 
     # a TMA can only be defined by 2 FOVs: an upper-left corner and a bottom-right corner
-    if len(fov_list_info['fovs']) != 2:
+    if len(tma_corners['fovs']) != 2:
         raise ValueError("Your FOV region file %s needs to contain only 2 FOVs" % tma_corners_path)
 
     # retrieve the corner FOVs
     # NOTE: the upper-left should always be listed before the bottom-right
-    upper_left = fov_list_info['fovs'][0]
-    bottom_right = fov_list_info['fovs'][1]
+    upper_left = tma_corners['fovs'][0]
+    bottom_right = tma_corners['fovs'][1]
 
     # define the start and end coordinates
     start_fov_x = upper_left['centerPointMicrons']['x']
