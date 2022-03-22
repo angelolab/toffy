266a4adc52de8adebbcd649a30d2fc028bfaa407
ackagel <ackagel@ucdavis.edu>
2022-03-22 11:56:32 -0700

    adds docstring to callback generator

--- toffy/watcher_callbacks.py
+++ toffy/watcher_callbacks.py
@@ -1,11 +1,32 @@
 import os
+from typing import Union, Tuple
+
+import pandas as pd
 
 from mibi_bin_tools.bin_files import extract_bin_files
 
 from toffy.qc_comp import compute_qc_metrics
 
 
-def build_extract_and_compute_qc_callback(panel, extraction_dir_name='extracted', **kwargs):
+def build_extract_and_compute_qc_callback(panel: Union[Tuple[float, float], pd.DataFrame],
+                                          extraction_dir_name: str = 'extracted', **kwargs):
+    """Genereates extraction and qc metric computation callback for given panel + parameters
+
+    Args:
+        panel (tuple | pd.DataFrame):
+            Target mass integration ranges
+        extraction_dir_name (str):
+            Subdirectory to place extracted TIFs into
+        **kwargs (dict):
+            Additional arguments for `mibi_bin_tools.bin_files.extract_bin_files` and
+            `toffy.qc_comp.compute_qc_metrics`.  Accepted kwargs are:
+             - intensities
+             - time_res
+             - channels
+             - gaussian_blur
+             - blur_factor
+             - dtype
+    """
 
     intensities = kwargs.pop('intensities', False)
     time_res = kwargs.pop('time_res', 0.005)
