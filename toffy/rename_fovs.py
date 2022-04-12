422e7bfbfe5df27024a8c714cfa6386f2ae54643
Cami PC <camisowers@hotmail.com>
2022-04-12 00:24:32 -0700

    get fov names and scan counts

--- toffy/rename_fovs.py
+++ toffy/rename_fovs.py
@@ -14,3 +14,11 @@ def rename_fov_dirs(run_dir, fov_dir, new_dir=FALSE):
     Returns:
         """
 
+    io_utils.validate_paths(run_dir)
+    io_utils.validate_paths(run_dir)
+
+    #retieve FOV names and number of scans for each
+    with open(run_dir) as f:
+        data = json.load(f)
+        run_data = data['fovs']
+        fov_scan = {x['name']: x['scanCount'] for x in run_data}
