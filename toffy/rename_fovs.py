8c758deb1b94393febb4273633bac433d79a5963
Cami PC <camisowers@hotmail.com>
2022-04-15 13:58:09 -0700

    option to copy to new directory and rename

--- toffy/rename_fovs.py
+++ toffy/rename_fovs.py
@@ -1,16 +1,17 @@
 import json
 import os
+from distutils.dir_util import copy_tree
 
 from ark.utils import io_utils
 
 
-def rename_fov_dirs(run_path, fov_dir, new_dir=False):
+def rename_fov_dirs(run_path, fov_dir, new_dir=None):
     """Renames FOV directories to have specific names sources from the run JSON file
 
     Args:
         run_path (str): path to the JSON run file
         fov_dir (str): directory where the FOV subdirectories are stored
-        new_dir (bool): whether or not to output renamed folders to a new directory
+        new_dir (str): name of new directory to output files to, defaults to None
 
         """
 
@@ -45,13 +46,23 @@ def rename_fov_dirs(run_path, fov_dir, new_dir=False):
     if name_count != dir_count:
         raise ValueError(f"FOV folders do not match expected amount listed in the run file")
 
+    # create new directory and copy contents of fov_dir
+    if new_dir is not None:
+        parent = os.path.join(fov_dir, os.pardir)
+        new_dir = os.path.join(parent, new_dir)
+        copy_tree(fov_dir, new_dir)
+
     #change the FOV directory names
     renamed_dirs = 0
     for fov in fov_scan:
         scan_num = fov_scan[fov]
         for scan in range(1, scan_num+1):
-            fov_subdir = os.path.join(fov_dir, old_dirs[renamed_dirs])
-            new_name = os.path.join(fov_dir, fov+"-scan-"+str(scan))
+            if new_dir is not None:
+                change_dir = new_dir
+            else:
+                change_dir = fov_dir
+            fov_subdir = os.path.join(change_dir, old_dirs[renamed_dirs])
+            new_name = os.path.join(change_dir, fov + "-scan-" + str(scan))
             os.rename(fov_subdir, new_name)
             renamed_dirs += 1
 
@@ -59,4 +70,5 @@ def rename_fov_dirs(run_path, fov_dir, new_dir=False):
 '''
 r = os.path.join("data", "json_test", "2022-04-07_TONIC_TMA21_run1.json")
 f = os.path.join("data", "fov_folders")
-rename_fov_dirs(r, f)'''
+n = 'new_directory_name'
+rename_fov_dirs(r, f, n)'''
