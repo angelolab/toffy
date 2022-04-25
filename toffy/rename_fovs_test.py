585755b1f624a942c23e0dc1d89fb21718a75c91
Cami PC <camisowers@hotmail.com>
2022-04-24 22:35:45 -0700

    add general function to create run metadata

--- toffy/rename_fovs_test.py
+++ toffy/rename_fovs_test.py
@@ -1,29 +1,51 @@
-# import tempfile
+import tempfile
+import json
 
 # from ark.utils import test_utils
 from toffy import rename_fovs
 
 
+def create_sample_run(name_list, run_order_list, scan_count_list, create_json=False):
+    """Creates example run metadata, create temporary JSON file if required
+       Args:
+           lists
+           create_json (bool): whether to create temporary JSON file and return the path, defaults to False
+
+       Returns:
+           dict: correctly formatted dictionary of sample run metadata
+           path: the path to a temporary JSON file for the sample run
+          """
+    fov_list = []
+    sample_run = {"fovs": fov_list}
+
+    for name, run_order, scan_count in zip(name_list, run_order_list, scan_count_list):
+        ex_fov = {
+            "scanCount": scan_count,
+            "runOrder": run_order,
+            "name": name
+        }
+        fov_list.append(ex_fov)
+
+    for fov in sample_run.get('fovs', ()):
+        if fov.get('name') == 'missing':
+            del fov['name']
+
+    if create_json:
+        temp = tempfile.NamedTemporaryFile(mode="w")
+        json.dump(sample_run, temp)
+        print(temp.name)
+        return temp.name
+
+    return sample_run
+
+
 def test_check_unnamed_fovs():
-    ex_name = ['fov-1', 'fov-2', 'fov-3', 'fov-4']
-    ex_run_order = [1, 2, 3, 4, 5]
-    ex_scan_count = [1, 2, 3, 4, 5]
-    ex_fov_list = []
-    sample_fov_scan = {"fovs": ex_fov_list}
+    ex_name = ['fov-1', 'fov-2', 'fov-3', 'fov-4', 'missing']
+    ex_run_order = list(range(1, 6))
+    ex_scan_count = list(range(1, 6))
+    ex_run = create_sample_run(ex_name, ex_run_order, ex_scan_count)
 
-    for run_order, scan_count in zip(ex_run_order, ex_scan_count):
-        if not run_order > len(ex_name):
-            ex_fov = {
-                "scanCount": scan_count,
-                "runOrder": run_order,
-                "name": ex_name[run_order-1]
-            }
-        else:
-            ex_fov = {
-                "scanCount": scan_count,
-                "runOrder": run_order,
-            }
-        ex_fov_list.append(ex_fov)
+    rename_fovs.check_unnamed_fovs(ex_run)
 
-    print(sample_fov_scan)
-    rename_fovs.check_unnamed_fovs(sample_fov_scan)
+
+test_check_unnamed_fovs()
