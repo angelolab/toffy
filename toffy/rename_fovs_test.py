ea37cb2481c0ad214056ca35eba129c15996fea6
Cami PC <camisowers@hotmail.com>
2022-04-26 11:38:32 -0700

    add test for bad run file

--- toffy/rename_fovs_test.py
+++ toffy/rename_fovs_test.py
@@ -9,7 +9,7 @@ from ark.utils import io_utils
 from toffy import rename_fovs as rf
 
 
-def create_sample_run(data, create_json=False):
+def create_sample_run(data, create_json=False, bad=False):
     fov_list = []
     sample_run = {"fovs": fov_list}
 
@@ -28,6 +28,11 @@ def create_sample_run(data, create_json=False):
         if fov.get('name') is None:
             del fov['name']
 
+    # create bad dictionary
+    if bad:
+        sample_run['bad key'] = sample_run['fovs']
+        del sample_run['fovs']
+
     # create json file for the data
     if create_json:
         temp = tempfile.NamedTemporaryFile(mode="w", delete=False)
@@ -39,11 +44,13 @@ def create_sample_run(data, create_json=False):
 
 
 def create_sample_fov_dirs(fovs, base_dir):
+    # make fov subdirectories
     for fov in fovs:
         os.mkdir(os.path.join(base_dir, fov))
 
 
 def remove_fov_dirs(base_dir):
+    # delete fov subdirectories
     fovs = io_utils.list_folders(base_dir)
     for fov in fovs:
         os.rmdir(os.path.join(base_dir, fov))
@@ -83,8 +90,6 @@ def test_rename_fov_dirs():
         with pytest.raises(ValueError, match="already exists"):
             rf.rename_fov_dirs(run_dir, fov_dir, not_new_dir)
 
-        # bad sample run data
-
         # regular sample run data
         ex_data = pd.DataFrame(
             {'names': ['MoQC', 'tonsil_bottom', 'moly_qc_tissue'],
@@ -94,6 +99,13 @@ def test_rename_fov_dirs():
 
         # create a json path with the sample data
         ex_run_path = create_sample_run(ex_data, True)
+
+        # bad sample run data
+        bad_run = create_sample_run(ex_data, True, bad=True)
+
+        # bad run file data should raise an error
+        with pytest.raises(KeyError):
+            rf.rename_fov_dirs(bad_run, fov_dir)
 
         # create already renamed fov folders
         renamed_fovs = ['MoQC', 'tonsil_bottom-1', 'tonsil_bottom-2', 'moly_qc_tissue']
