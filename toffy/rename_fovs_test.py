7100e8b01a90ce81e3d7713d9eb2802e6cd0eefd
Cami PC <camisowers@hotmail.com>
2022-04-26 18:12:40 -0700

    change back to 3 lists and renames fov names as custom_1

--- toffy/rename_fovs_test.py
+++ toffy/rename_fovs_test.py
@@ -2,20 +2,17 @@ import tempfile
 import json
 import os
 import pytest
-import pandas as pd
 
-# from ark.utils import test_utils
 from ark.utils import io_utils
 from toffy import rename_fovs as rf
 
 
-def create_sample_run(data, create_json=False, bad=False):
+def create_sample_run(name_list, run_order_list, scan_count_list, create_json=False, bad=False):
     fov_list = []
     sample_run = {"fovs": fov_list}
 
     # set up dictionary
-    for name, run_order, scan_count in \
-            zip(data.loc[:, "names"], data.loc[:, "run"], data.loc[:, "scans"]):
+    for name, run_order, scan_count in zip(name_list, run_order_list, scan_count_list):
         ex_fov = {
             "scanCount": scan_count,
             "runOrder": run_order,
@@ -57,14 +54,12 @@ def remove_fov_dirs(base_dir):
 
 def test_check_unnamed_fovs():
     # data with missing names
-    ex_data = pd.DataFrame(
-        {'names': ['MoQC', None, 'tonsil_bottom', 'moly_qc_tissue', None],
-         'run': list(range(1, 6)),
-         'scans': list(range(1, 6))
-         })
+    ex_name = ['custom_1', None, 'custom_2', 'custom_3', None]
+    ex_run_order = list(range(1, 6))
+    ex_scan_count = list(range(1, 6))
 
     # create a dict with the sample data
-    ex_run = create_sample_run(ex_data)
+    ex_run = create_sample_run(ex_name, ex_run_order, ex_scan_count)
 
     # test that missing names are given a placeholder
     rf.check_unnamed_fovs(ex_run)
@@ -90,24 +85,22 @@ def test_rename_fov_dirs():
             rf.rename_fov_dirs(run_dir, fov_dir, not_new_dir)
 
         # regular sample run data
-        ex_data = pd.DataFrame(
-            {'names': ['MoQC', 'tonsil_bottom', 'moly_qc_tissue'],
-             'run': list(range(1, 4)),
-             'scans': [1, 2, 1]
-             })
+        ex_name = ['custom_1', 'custom_2', 'custom_3']
+        ex_run_order = list(range(1, 4))
+        ex_scan_count = [1, 2, 1]
 
         # create a json path with the sample data
-        ex_run_path = create_sample_run(ex_data, True)
+        ex_run_path = create_sample_run(ex_name, ex_run_order, ex_scan_count, True)
 
         # bad sample run data
-        bad_run = create_sample_run(ex_data, True, bad=True)
+        bad_run = create_sample_run(ex_name, ex_run_order, ex_scan_count, True, bad=True)
 
         # bad run file data should raise an error
         with pytest.raises(KeyError):
             rf.rename_fov_dirs(bad_run, fov_dir)
 
         # create already renamed fov folders
-        renamed_fovs = ['MoQC', 'tonsil_bottom-1', 'tonsil_bottom-2', 'moly_qc_tissue']
+        renamed_fovs = ['custom_1', 'custom_2-1', 'custom_2-2', 'custom_3']
         create_sample_fov_dirs(renamed_fovs, fov_dir)
 
         # fov folders already renamed should raise an error
@@ -134,7 +127,7 @@ def test_rename_fov_dirs():
         less_fovs = ['fov-1-scan-1', 'fov-2-scan-1', 'fov-3-scan-1']
         create_sample_fov_dirs(less_fovs, fov_dir)
 
-        # fov folders already renamed should raise an error
+        # fovs in rule file without an existing dir should raise an error
         with pytest.warns(UserWarning, match="Not all FOVs"):
             rf.rename_fov_dirs(ex_run_path, fov_dir)
         remove_fov_dirs(fov_dir)
@@ -144,7 +137,7 @@ def test_rename_fov_dirs():
                       'fov-3-scan-1', 'fov-3-scan-3']
         create_sample_fov_dirs(extra_fovs, fov_dir)
 
-        # fov folders already renamed should raise an error
+        # extra dirs not listed in run file should raise an error
         with pytest.raises(ValueError, match="not found in run file"):
             rf.rename_fov_dirs(ex_run_path, fov_dir)
         remove_fov_dirs(fov_dir)
