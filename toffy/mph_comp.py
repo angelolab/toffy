aaa3e4b35fbe6512993a53513e9189cdf46d8243
Cami PC <camisowers@hotmail.com>
2022-05-16 19:00:37 -0700

    add default mass_range

--- toffy/mph_comp.py
+++ toffy/mph_comp.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from mibi_bin_tools import bin_files
 
 
-def compute_mph_metrics(bin_file_path, target, save_csv=True, mass_range=None,):
+def compute_mph_metrics(bin_file_path, target, save_csv=True, mass_range=(-0.3, 0.0)):
 
     # retrieve the total counts and compute pulse heights for each FOV run file
     # saves individual .csv  files to bin_file_path
@@ -16,12 +16,8 @@ def compute_mph_metrics(bin_file_path, target, save_csv=True, mass_range=None,):
     for i in range(1, len(total_counts) + 1):
         pulse_height_file = 'fov-{}-pulse_height.csv'.format(i)
 
-        if mass_range is None:
-            median = bin_files.get_median_pulse_height(bin_file_path,
-                                                       'fov-{}-scan-1'.format(i), target)
-        else:
-            median = bin_files.get_median_pulse_height(bin_file_path, 'fov-{}-scan-1'.format(i),
-                                                       target, mass_range)
+        median = bin_files.get_median_pulse_height(bin_file_path, 'fov-{}-scan-1'.format(i),
+                                                   target, mass_range)
         count = total_counts['fov-{}-scan-1'.format(i)]
 
         out_df = pd.DataFrame({
