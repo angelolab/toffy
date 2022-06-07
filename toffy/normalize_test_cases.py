d79adc5fd0c9573a9646c218860a038840946a1b
Noah F. Greenwald <noahfgreenwald@gmail.com>
2022-06-07 15:00:02 -0700

    Mph norm workflow curve (#123)
    * combine metrics doesn't require bins
    
    * helper function for formatting df
    
    * refactor saving function
    
    * fov helper function
    
    * refactored top level function
    
    * pycodestyle
    
    * use median for small datasets
    
    * update notebook
    
    * close plots
    
    * fix bug in df construction
    
    * sort df by channels
    
    * outlier identification
    
    * plot outliers separately
    
    * remove outlier functionality
    
    * change outlier detection
    
    * updated docstrings
    
    * make test more robust

--- toffy/normalize_test_cases.py
+++ toffy/normalize_test_cases.py
@@ -3,7 +3,10 @@ import pytest
 import numpy as np
 import pandas as pd
 
-masses = np.arange(5, 20)
+masses = np.arange(5, 15)
+channels = ['chan_{}'.format(i) for i in range(len(masses))]
+panel = pd.DataFrame({'Mass': masses, 'Target': channels})
+fovs = ['fov{}'.format(12 - i) for i in range(4)]
 
 
 class TuningCurveFiles:
@@ -34,18 +37,15 @@ class TuningCurveFiles:
         return dirs, mph_dfs, count_dfs
 
 
-class CombineRunMetricFiles():
+class CombineRunMetricFiles:
 
     def case_default_metrics(self):
         # create full directory of files
-        bins = []
         metrics = []
-        for i in range(1, 5):
-            bin_name = 'example_{}.bin'.format(i)
-            bins.append(bin_name)
-            metric_name = 'example_metric_{}.csv'.format(i)
-            metric_values = {'column_1': np.random.rand(10),
-                             'column_2': np.random.rand(10),
-                             'column_3': np.random.rand(10)}
+        for i in range(0, 4):
+            metric_name = 'pulse_heights_{}.csv'.format(i)
+            metric_values = {'pulse_height': np.random.rand(10),
+                             'mass': masses,
+                             'fov': [fovs[i]] * 10}
             metrics.append([metric_name, metric_values])
-        return bins, metrics
+        return metrics
