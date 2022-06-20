e5f38363ddf24125c808fa61bd9ceae708fe7ce9
Cami PC <camisowers@hotmail.com>
2022-06-20 11:09:58 -0700

    test for success

---
+++ toffy/bin_extraction_test.py
@@ -0,0 +1,32 @@
+import os
+import tempfile
+
+from pathlib import Path
+
+from toffy import bin_extraction
+from ark.utils import io_utils
+
+
+def test_extract_missing_fovs():
+    panel = {'Mass': 98, 'Target': None, 'Mass start': 97.5, 'Mass stop': 97.5}
+
+    # test successful extraction
+    bin_file_dir = os.path.join(Path(__file__).parent, "data", "tissue")
+    with tempfile.TemporaryDirectory() as extraction_dir:
+        bin_extraction.extract_missing_fovs(bin_file_dir, extraction_dir,
+                                            panel, extract_intensities=False)
+        fovs = io_utils.list_folders(bin_file_dir)
+        fovs_extracted = io_utils.list_folders(extraction_dir)
+
+        assert fovs == fovs_extracted
+
+    # test that moly fovs are not extracted
+    moly_bin_file_dir = os.path.join(Path(__file__).parent, "data", "moly")
+    with tempfile.TemporaryDirectory() as tmpdir:
+        extraction_dir = tmpdir
+
+        bin_extraction.extract_missing_fovs(moly_bin_file_dir, extraction_dir,
+                                            panel, extract_intensities=False)
+        fovs_extracted = io_utils.list_folders(extraction_dir)
+
+        assert len(fovs_extracted) == 0
