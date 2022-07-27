4264e7feef74ef89e343c685a647be74ce047ac6
mheskett <mbheskett@gmail.com>
2022-07-27 12:38:17 -0700

    cleaned up code

--- toffy/qc_metrics_plots.py
+++ toffy/qc_metrics_plots.py
@@ -6,67 +6,57 @@ from toffy import qc_comp
 from ark.utils import io_utils
 import pandas as pd
 
-def load_normal_fovs(data_dir, 
-	                 normal_tissues,
-                     channels=None,
-                     exclude_channels=None,
-                     img_sub_folder=None):
-    
-    normal_samples = io_utils.list_folders(dir_name=data_dir, substrs=normal_tissues)
-    data = load_imgs_from_tree(data_dir=data_dir, 
-	                    img_sub_folder=img_sub_folder, 
-	                    fovs=normal_samples, 
-	                    channels=channels)    
-    return data
 
-def qc_metrics_plots(data_dir, 
-	                 normal_tissues,
-                     channels=None,
-                     exclude_channels=None,
-	                 qc_metric="99.9th_percentile",
-	                 fig_dir=None):
-    
-    normal_samples = io_utils.list_folders(dir_name=data_dir, substrs=normal_tissues)
+def call_violin_swarm_plot(plotting_df, fig_label, figsize=(20, 3), fig_dir=None):
+  
+    plt.figure(figsize=figsize)
+    ax = sns.violinplot(x="channel", y="99.9th_percentile", data=plotting_df,
+                        inner=None, scale="width", color="gray")
+    ax = sns.swarmplot(x="channel", y="99.9th_percentile", data=plotting_df,
+                       edgecolor="black", hue="tma", palette="tab20")
+    ax.set_title(fig_label)
+    plt.xticks(rotation=45)
+    if fig_dir:
+        plt.savefig(fig_dir+fig_label+"_batch_effects.png", dpi=300)
+    plt.show()
+    plt.close()
 
-    # need to make a nice pandas DF with columns: sample, channel, 99.9th value
+    return
+
+
+def make_batch_effect_plot(data_dir,
+                           normal_tissues,
+                           exclude_channels=None,
+                           img_sub_folder=None,
+                           qc_metric="99.9th_percentile",
+                           fig_dir=None):
+
     for i in range(len(normal_tissues)):
-        data = load_normal_fovs(data_dir,
-                                normal_tissues=normal_tissues,
-                                channels=channels)
+        samples = io_utils.list_folders(dir_name=data_dir, substrs=normal_tissues[i])
+        data = load_imgs_from_tree(data_dir=data_dir,
+                                   img_sub_folder=img_sub_folder,
+                                   fovs=samples)
         channels = list(data.channels.values)
         if exclude_channels:
             channels = [x for x in channels if x not in exclude_channels]
-        
-        
-        # For each tissue type, get all the samples
-        samples = [x for x in normal_samples if normal_tissues[i] in x]
-        plotting_df = pd.DataFrame(columns=["sample",'channel',"tma","99.9th_percentile"])
-        
+        plotting_df = pd.DataFrame(columns=["sample", "channel", "tma", "99.9th_percentile"])
+   
         for j in range(len(channels)):
-            # Channel is a gene
             qc_metrics_per_channel = []
 
             for k in range(len(samples)):
-                # go through all samples per gene. list should be of length==samples
                 tma = [x for x in samples[k].split("_") if "TMA" in x][0]
                 qc_metrics_per_channel += [[normal_tissues[i],
                                            channels[j],
                                            tma,
-                                           qc_comp.compute_99_9_intensity(data.loc[samples[k], 0:1023, 0:1023, channels[j]])]]
-            
+                                           qc_comp.compute_99_9_intensity(data.loc[samples[k],
+                                                                          :,
+                                                                          :,
+                                                                          channels[j]])]]
 
-            plotting_df = plotting_df.append( pd.DataFrame(qc_metrics_per_channel,columns=plotting_df.columns) )
-        plt.figure(figsize=(20,3))
-        ax = sns.violinplot(x="channel",y="99.9th_percentile",data=plotting_df,
-                            inner=None,scale="width",color="gray")
-        ax = sns.swarmplot(x="channel",y="99.9th_percentile",
-                           data=plotting_df,
-                           edgecolor="black",hue="tma",palette="tab20")
-        ax.set_title(normal_tissues[i])
-        plt.xticks(rotation = 45)
-        if fig_dir:
-            plt.savefig(fig_dir+normal_tissues[i]+"_qc.png",dpi=300)
-        plt.show()
-        plt.close()
-        
+            plotting_df = plotting_df.append(pd.DataFrame(qc_metrics_per_channel,
+                                                          columns=plotting_df.columns))
+
+        call_violin_swarm_plot(plotting_df, fig_label=normal_tissues[i], fig_dir=fig_dir)
+   
     return
