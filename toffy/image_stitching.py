8394c9271024ffcbd4315ff0072f3488120ab997
camisowers <camisowers@g.ucla.edu>
2022-07-27 12:22:03 -0700

    testing and comments

--- toffy/image_stitching.py
+++ toffy/image_stitching.py
@@ -11,19 +11,31 @@ from mibi_bin_tools.io_utils import remove_file_extensions
 
 # helper function that scans the run json for max image size, key = frameSize
 def get_max_img_size(run_dir):
+    """Retrieves the maximum FOV image size listed in the fun file
+        Args:
+            run_dir (str): path to the run directory containing the run json files """
+
     run_name = os.path.basename(run_dir)
     run_file_path = os.path.join(run_dir, run_name + '.json')
 
+    # retrieve all pixel width dimensions of the fovs
     run_data = json_utils.read_json_file(run_file_path)
     img_sizes = []
     for fov in run_data['fovs']:
         img_sizes.append(fov.get('frameSizePixels')['width'])
 
+    # return largest
     max_img_size = max(img_sizes)
+
     return max_img_size
 
 
 def stitch_images(tiff_out_dir, run_dir, channels=None):
+    """Creates a new directory containing stitched channel images for the run
+        Args:
+            tiff_out_dir (str): path to the extracted images
+            run_dir (str): path to the run directory containing the run json files
+            channels (list): list of channels to produce stitched images for, None will do all """
 
     # remove old images
     stitched_dir = os.path.join(tiff_out_dir, 'stitched_images')
@@ -33,6 +45,7 @@ def stitch_images(tiff_out_dir, run_dir, channels=None):
     folders = io_utils.list_folders(tiff_out_dir)
     folders = ns.natsorted(folders)
 
+    # retrieve all extracted channel names, or verify the list provided
     if channels is None:
         channels = remove_file_extensions(io_utils.list_files(
             dir_name=os.path.join(tiff_out_dir, folders[0]), substrs='.tiff'))
@@ -40,17 +53,17 @@ def stitch_images(tiff_out_dir, run_dir, channels=None):
         misc_utils.verify_in_list(channel_inputs=channels, valid_channels=remove_file_extensions(
             io_utils.list_files(dir_name=os.path.join(tiff_out_dir, folders[0]), substrs='.tiff')))
 
+    # load in and stitch the image data
     num_cols = math.isqrt(len(folders))
     max_img_size = get_max_img_size(run_dir)
-
     image_data = load_utils.load_imgs_from_tree(tiff_out_dir, fovs=folders, channels=channels,
                                                 max_image_size=max_img_size, dtype='uint32')
-
     stitched = data_utils.stitch_images(image_data, num_cols)
 
     # recreate directory
     os.makedirs(stitched_dir)
 
+    # save the stitched images to the stitched_image subdir
     for chan in stitched.channels.values:
         current_img = stitched.loc['stitched_image', :, :, chan].values
         io.imsave(os.path.join(stitched_dir, chan + '.tiff'), current_img.astype('uint8'),
--- toffy/image_stitching_test.py
+++ toffy/image_stitching_test.py
@@ -6,7 +6,17 @@ from toffy import image_stitching
 from ark.utils import io_utils, test_utils
 
 
-def test_stitch_images():
+def test_get_max_img_size():
+    run_dir = os.path.join('data', 'tissue')
+
+    #test sucess
+    max_img_size = image_stitching.get_max_img_size(run_dir)
+    assert max_img_size == 32
+
+
+def test_stitch_images(mocker):
+    mocker.patch('toffy.image_stitching.get_max_img_size', return_value=32)
+
     with tempfile.TemporaryDirectory() as tmpdir:
         channel_list = ['Au', 'CD3', 'CD4', 'CD8', 'CD11c']
         stitched_tifs = ['Au.tiff', 'CD3.tiff', 'CD4.tiff', 'CD8.tiff', 'CD11c.tiff']
@@ -16,14 +26,14 @@ def test_stitch_images():
         # bad channel should raise an error
         with pytest.raises(ValueError, match='Not all values given in list channel inputs were '
                                              'found in list valid channels.'):
-            image_stitching.stitch_images(tmpdir, ['Au', 'bad_channel'])
+            image_stitching.stitch_images(tmpdir, tmpdir, ['Au', 'bad_channel'])
 
         # test successful stitching for all channels
-        image_stitching.stitch_images(tmpdir)
+        image_stitching.stitch_images(tmpdir, tmpdir)
         assert sorted(io_utils.list_files(os.path.join(tmpdir, 'stitched_images'))) == \
                sorted(stitched_tifs)
 
         # test stitching for specific channels
-        image_stitching.stitch_images(tmpdir, ['Au', 'CD3'])
+        image_stitching.stitch_images(tmpdir, tmpdir, ['Au', 'CD3'])
         assert sorted(io_utils.list_files(os.path.join(tmpdir, 'stitched_images'))) == \
                sorted(['Au.tiff', 'CD3.tiff'])
