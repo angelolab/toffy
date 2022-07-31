e78b4b015b9582f88cf3694e212032b0762bc082
mheskett <mbheskett@gmail.com>
2022-07-31 14:37:15 -0700

    wrapped test function

--- toffy/qc_metrics_plots_test.py
+++ toffy/qc_metrics_plots_test.py
@@ -10,25 +10,24 @@ import os
 import qc_metrics_plots
 
 
+def make_batch_effect_plot_test():
+	with tempfile.TemporaryDirectory() as top_level_dir:
+	    data_dir = os.path.join(top_level_dir, 'data_dir')
+	    os.makedirs(data_dir)
 
-with tempfile.TemporaryDirectory() as top_level_dir:
-    data_dir = os.path.join(top_level_dir, 'data_dir')
-    os.makedirs(data_dir)
-
-    # make fake data for testing
-    # file locs are the temp files
-    # data_xr is the image data xarray
-    fovs, chans = test_utils.gen_fov_chan_names(num_fovs=3, num_chans=3)
-    chans=["chan0","chan1","chan2"]
-    fovs=["TMA1_fov1","TMA1_fov2","TMA2_fov1","TMA2_fov2"]
-    filelocs, data_xr = test_utils.create_paired_xarray_fovs(
-        data_dir, fovs, chans, img_shape=(10, 10), fills=True)
-    
-    print(filelocs)
-    # no errors
-    qc_metrics_plots.make_batch_effect_plot(data_dir=data_dir,
-                           normal_tissues=["fov1"],
-                           exclude_channels=None,
-                           img_sub_folder=None,
-                           qc_metric="99.9th_percentile",
-                           fig_dir=None)
+	    # make fake data for testing
+	    # file locs are the temp files
+	    # data_xr is the image data xarray
+	    fovs, chans = test_utils.gen_fov_chan_names(num_fovs=3, num_chans=3)
+	    chans=["chan0","chan1","chan2"]
+	    fovs=["TMA1_fov1","TMA1_fov2","TMA2_fov1","TMA2_fov2"]
+	    filelocs, data_xr = test_utils.create_paired_xarray_fovs(
+	        data_dir, fovs, chans, img_shape=(10, 10), fills=True)
+	    
+	    # no errors
+	    qc_metrics_plots.make_batch_effect_plot(data_dir=data_dir,
+	                           normal_tissues=["fov1"],
+	                           exclude_channels=None,
+	                           img_sub_folder=None,
+	                           qc_metric="99.9th_percentile",
+	                           fig_dir=None)
