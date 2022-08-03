eee8c6f4595b844dd619b4d504ed325ace644b70
camisowers <camisowers@g.ucla.edu>
2022-08-03 12:21:21 -0700

    match issue

--- toffy/fov_watcher_test.py
+++ toffy/fov_watcher_test.py
@@ -56,13 +56,12 @@ def test_run_structure(run_json, expected_files):
         assert all(run_structure.check_fov_progress().values())
 
         # check for hidden files
-        with pytest.warns(UserWarning, match=r"*. is not a valid FOV file and will be skipped"
-                                              r"from processing."):
+        with pytest.warns(UserWarning, match="is not a valid FOV file and will be skipped"):
             exist, name = run_structure.check_run_condition(os.path.join(tmpdir, '.fake_file.txt'))
         assert not exist and name == ''
 
         # check for fake files
-        with pytest.warns(UserWarning, match=r"*. This should be unreachable..."):
+        with pytest.warns(UserWarning, match="This should be unreachable..."):
             exist, name = run_structure.check_run_condition(os.path.join(tmpdir, 'fake_file.txt'))
         assert not exist and name == ''
 
