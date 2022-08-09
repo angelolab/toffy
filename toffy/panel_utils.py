509fb89395ab40122514af8ada3875d3843807cd
camisowers <camisowers@g.ucla.edu>
2022-08-09 13:04:04 -0700

    original panel mass duplicate check

--- toffy/panel_utils.py
+++ toffy/panel_utils.py
@@ -1,6 +1,8 @@
 import os
 import pandas as pd
 
+from pathlib import Path
+
 from ark.utils import io_utils
 
 
@@ -12,7 +14,7 @@ def convert_panel(panel_path):
         panel_path (str): direct path to panel file
     Returns:
         panel (pd.DataFrame): detailing the Mass, Target, Start, and Stop values
-        also saves the above to file with name formatted "panel_name-toffy.csv'
+        also saves the above to file with name formatted "panel_name-toffy.csv"
     """
 
     # retrieve the panel name and directory it is contained in
@@ -20,10 +22,10 @@ def convert_panel(panel_path):
     panel_dir = os.path.dirname(panel_path)
 
     # read in the example_panel_file to have necessary additional targets
-    example_panel = pd.read_csv(os.path.join('..', 'files', 'example_panel_file.csv'),
-                                index_col=False)
+    example_panel = pd.read_csv(os.path.join(Path(__file__).parent.parent, 'files',
+                                             'example_panel_file.csv'), index_col=False)
 
-    # read in file to avoid metadata contained in the first few lines
+    # read in metadata contained in the first few lines
     with open(panel_path) as r:
         lines = r.readlines()
         for index, line in enumerate(lines):
@@ -45,21 +47,31 @@ def convert_panel(panel_path):
     panel.columns = panel.columns.get_level_values(0)
     toffy_panel = panel[['Mass', 'Target']].copy()
 
-    # edit panel to add Start and Stop values
     mass_start = []
     mass_stop = []
 
     for i, row in toffy_panel.iterrows():
         mass = row['Mass']
-        mass_start.append(mass-0.3)
-        mass_stop.append(mass)
+        # check for different targets on same mass
+        if mass in mass_stop:
+            dup_index = mass_stop.index(mass)
+            # concatenate target names
+            target_name = toffy_panel['Target'][dup_index] + '_' + row['Target']
+            toffy_panel.loc[toffy_panel['Mass'] == mass, 'Target'] = target_name
+        else:
+            mass_start.append(mass - 0.3)
+            mass_stop.append(mass)
 
+    # get rid of one of the duplicate mass rows
+    toffy_panel = toffy_panel.drop_duplicates(subset=['Mass'], keep='first')
+
+    # edit panel to add Start and Stop values
     toffy_panel['Start'] = mass_start
     toffy_panel['Stop'] = mass_stop
 
-    # add example panel values to new toffy panel, not keeping any duplicates
+    # add example panel targets to new toffy panel, removing any duplicates
     toffy_panel = pd.concat([toffy_panel, example_panel], ignore_index=True)
-    toffy_panel = toffy_panel.drop_duplicates(subset=['Target'])
+    toffy_panel = toffy_panel.drop_duplicates(subset=['Mass'], keep='first')
 
     toffy_panel.to_csv(os.path.join(panel_dir, panel_name + '-toffy.csv'), index=False)
 
--- toffy/panel_utils_test.py
+++ toffy/panel_utils_test.py
@@ -10,16 +10,16 @@ from ark.utils import test_utils
 def test_convert_panel():
     with tempfile.TemporaryDirectory() as temp_dir:
         test_panel = pd.DataFrame({
-            'ID (Lot)': [1352, 1350, 1351, 0],
-            'Target': ['Calprotectin', 'Chymase', 'Mast Cell Tryptase', 'Au'],
-            'Clone': ['MAC387', 'EPR13136', 'EPR9522', ''],
-            'Mass': [69, 71, 89, 197],
-            'Element': ['Ga', 'Ga', 'Y', ''],
-            'Manufacture': ['7/20/20', '7/20/20', '5/5/21', ''],
-            'Stock': [200, 200, 200, 0],
-            'Titer': [0.125, 0.125, 0.25, 0],
+            'ID (Lot)': [1352, 1352, 1350, 1351],
+            'Target': ['Calprotectin', 'Duplicate', 'Chymase', 'Mast Cell Tryptase'],
+            'Clone': ['MAC387', 'MAC387', 'EPR13136', 'EPR9522'],
+            'Mass': [69, 69, 71, 89],
+            'Element': ['Ga', 'Ga', 'Ga', 'Y'],
+            'Manufacture': ['7/20/20', '7/20/20', '7/20/20', '5/5/21'],
+            'Stock': [200, 200, 200, 200],
+            'Titer': [0.125, 0.125, 0.125, 0.25],
             'Volume': [0, 0, 0, 0],
-            'Staining Batch': [1, 1, 1, 0]
+            'Staining Batch': [1, 1, 1, 1]
         })
         test_panel.to_csv(os.path.join(temp_dir, 'test_panel.csv'), index=False)
 
@@ -43,15 +43,21 @@ def test_convert_panel():
         converted_panel = pd.read_csv(os.path.join(temp_dir, 'test_panel-toffy.csv'))
         necessary_panel = pd.read_csv(os.path.join('..', 'files', 'example_panel_file.csv'))
 
-        # check panel structure
+        # check toffy panel structure
         assert list(converted_panel.columns) == ['Mass', 'Target', 'Start', 'Stop']
 
-        # check Au valid is not duplicated when in both the original and necessary panel
-        assert len(list(converted_panel)) == len(set(converted_panel))
+        # check chan_71 is not duplicated
+        assert ['chan_71', 'Duplicate'] not in list(converted_panel['Target'])
 
-        # check for all targets
-        assert all(target in list(converted_panel['Target'])
-                   for target in (list(necessary_panel['Target'])))
+        # check concatenated target names for same mass
+        assert 'Calprotectin_Duplicate' in list(converted_panel['Target'])
+
+        # check for unique mass values
+        assert len(list(converted_panel['Mass'])) == len(set(converted_panel['Mass']))
+
+        # check for all necessary masses
+        assert all(mass in list(converted_panel['Mass'])
+                   for mass in (list(necessary_panel['Mass'])))
 
 
 def mock_panel_conversion(panel_path):
@@ -65,7 +71,7 @@ def mock_panel_conversion(panel_path):
     return toffy_panel
 
 
-def test_laod_panel(mocker):
+def test_load_panel(mocker):
     mocker.patch('toffy.panel_utils.convert_panel', mock_panel_conversion)
     toffy_panel = pd.DataFrame({
         'Target': ['Calprotectin', 'Chymase', 'Mast Cell Tryptase'],
@@ -80,8 +86,10 @@ def test_laod_panel(mocker):
         assert panel.equals(toffy_panel)
 
     with tempfile.TemporaryDirectory() as temp_dir:
-        # check that previously converted panel is loaded
+        # toffy panel
         toffy_panel.to_csv(os.path.join(temp_dir, 'test_panel-toffy.csv'), index=False)
+
+        # check that previously converted panel is loaded
         panel = panel_utils.load_panel(os.path.join(temp_dir, 'test_panel-toffy.csv'))
         assert panel.equals(toffy_panel)
 
