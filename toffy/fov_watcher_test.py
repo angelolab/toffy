297c1d03ba6a66c3d38f13a2615ae9e9c14fedb7
csowers <camisowers@g.ucla.edu>
2022-08-24 18:02:45 -0700

    catch OSError

--- toffy/fov_watcher_test.py
+++ toffy/fov_watcher_test.py
@@ -5,6 +5,7 @@ import tempfile
 from pathlib import Path
 from multiprocessing.pool import ThreadPool as Pool
 
+import warnings
 import pytest
 from pytest_cases import parametrize_with_cases
 from unittest.mock import patch
@@ -81,76 +82,80 @@ def test_run_structure(run_json, expected_files):
 @pytest.mark.parametrize('add_blank', [False, True])
 @parametrize_with_cases('run_cbs, fov_cbs, kwargs, validators', cases=WatcherCases)
 def test_watcher(mock_viz_qc, mock_viz_mph, run_cbs, fov_cbs, kwargs, validators, add_blank):
-    with tempfile.TemporaryDirectory() as tmpdir:
+    try:
+        with tempfile.TemporaryDirectory() as tmpdir:
 
-        tiff_out_dir = os.path.join(tmpdir, 'cb_0', RUN_DIR_NAME)
-        qc_out_dir = os.path.join(tmpdir, 'cb_1', RUN_DIR_NAME)
-        mph_out_dir = os.path.join(tmpdir, 'cb_2', RUN_DIR_NAME)
-        plot_dir = os.path.join(tmpdir, 'cb_2_plots', RUN_DIR_NAME)
-        stitched_dir = os.path.join(tmpdir, 'cb_0', RUN_DIR_NAME, 'stitched_images')
+            tiff_out_dir = os.path.join(tmpdir, 'cb_0', RUN_DIR_NAME)
+            qc_out_dir = os.path.join(tmpdir, 'cb_1', RUN_DIR_NAME)
+            mph_out_dir = os.path.join(tmpdir, 'cb_2', RUN_DIR_NAME)
+            plot_dir = os.path.join(tmpdir, 'cb_2_plots', RUN_DIR_NAME)
+            stitched_dir = os.path.join(tmpdir, 'cb_0', RUN_DIR_NAME, 'stitched_images')
 
-        # add directories to kwargs
-        kwargs['tiff_out_dir'] = tiff_out_dir
-        kwargs['qc_out_dir'] = qc_out_dir
-        kwargs['mph_out_dir'] = mph_out_dir
-        kwargs['plot_dir'] = plot_dir
+            # add directories to kwargs
+            kwargs['tiff_out_dir'] = tiff_out_dir
+            kwargs['qc_out_dir'] = qc_out_dir
+            kwargs['mph_out_dir'] = mph_out_dir
+            kwargs['plot_dir'] = plot_dir
 
-        run_data = os.path.join(tmpdir, 'test_run')
-        log_out = os.path.join(tmpdir, 'log_output')
-        os.makedirs(run_data)
+            run_data = os.path.join(tmpdir, 'test_run')
+            log_out = os.path.join(tmpdir, 'log_output')
+            os.makedirs(run_data)
 
-        fov_callback, run_callback = build_callbacks(run_cbs, fov_cbs, **kwargs)
-        write_json_file(json_path=os.path.join(run_data, 'test_run.json'),
-                        json_object=COMBINED_RUN_JSON_SPOOF)
+            fov_callback, run_callback = build_callbacks(run_cbs, fov_cbs, **kwargs)
+            write_json_file(json_path=os.path.join(run_data, 'test_run.json'),
+                            json_object=COMBINED_RUN_JSON_SPOOF)
 
-        # `_slow_copy_sample_tissue_data` mimics the instrument computer uploading data to the
-        # client access computer.  `start_watcher` is made async here since these processes
-        # wouldn't block each other in normal use
+            # `_slow_copy_sample_tissue_data` mimics the instrument computer uploading data to the
+            # client access computer.  `start_watcher` is made async here since these processes
+            # wouldn't block each other in normal use
 
-        with Pool(processes=4) as pool:
-            pool.apply_async(
-                _slow_copy_sample_tissue_data,
-                (run_data, SLOW_COPY_INTERVAL_S, add_blank)
-            )
+            with Pool(processes=4) as pool:
+                pool.apply_async(
+                    _slow_copy_sample_tissue_data,
+                    (run_data, SLOW_COPY_INTERVAL_S, add_blank)
+                )
 
-            # watcher completion is checked every second
-            # zero-size files are halted for 1 second or until they have non zero-size
-            res_scan = pool.apply_async(
-                start_watcher,
-                (run_data, log_out, fov_callback, run_callback, 1, SLOW_COPY_INTERVAL_S)
-            )
+                # watcher completion is checked every second
+                # zero-size files are halted for 1 second or until they have non zero-size
+                res_scan = pool.apply_async(
+                    start_watcher,
+                    (run_data, log_out, fov_callback, run_callback, 1, SLOW_COPY_INTERVAL_S)
+                )
 
-            res_scan.get()
+                res_scan.get()
 
-        with open(os.path.join(log_out, 'test_run_log.txt')) as f:
-            logtxt = f.read()
-            assert add_blank == ("non-zero file size..." in logtxt)
+            with open(os.path.join(log_out, 'test_run_log.txt')) as f:
+                logtxt = f.read()
+                assert add_blank == ("non-zero file size..." in logtxt)
 
-        fovs = [
-            bin_file.split('.')[0]
-            for bin_file in sorted(io_utils.list_files(COMBINED_DATA_PATH, substrs=['.bin']))
-        ]
+            fovs = [
+                bin_file.split('.')[0]
+                for bin_file in sorted(io_utils.list_files(COMBINED_DATA_PATH, substrs=['.bin']))
+            ]
 
-        # callbacks are not performed on moly points, remove fov-3-scan-1
-        bad_fovs = [fovs[-1]]
-        fovs = fovs[:-1]
+            # callbacks are not performed on moly points, remove fov-3-scan-1
+            bad_fovs = [fovs[-1]]
+            fovs = fovs[:-1]
 
-        # callbacks are not performed for skipped fovs, remove blank fov
-        if add_blank:
-            bad_fovs.append(fovs[0])
-            fovs = fovs[1:]
+            # callbacks are not performed for skipped fovs, remove blank fov
+            if add_blank:
+                bad_fovs.append(fovs[0])
+                fovs = fovs[1:]
 
-        # extract tiffs check
-        validators[0](os.path.join(tmpdir, 'cb_0', RUN_DIR_NAME), fovs, bad_fovs)
+            # extract tiffs check
+            validators[0](os.path.join(tmpdir, 'cb_0', RUN_DIR_NAME), fovs, bad_fovs)
 
-        # qc check
-        validators[1](os.path.join(tmpdir, 'cb_1', RUN_DIR_NAME), fovs, bad_fovs)
+            # qc check
+            validators[1](os.path.join(tmpdir, 'cb_1', RUN_DIR_NAME), fovs, bad_fovs)
 
-        # mph check
-        validators[2](os.path.join(tmpdir, 'cb_2', RUN_DIR_NAME),
-                      os.path.join(tmpdir, 'cb_2_plots', RUN_DIR_NAME), fovs, bad_fovs)
+            # mph check
+            validators[2](os.path.join(tmpdir, 'cb_2', RUN_DIR_NAME),
+                        os.path.join(tmpdir, 'cb_2_plots', RUN_DIR_NAME), fovs, bad_fovs)
 
-        # stitch images check
-        validators[3](os.path.join(tmpdir, 'cb_0', RUN_DIR_NAME, 'stitched_images'))
+            # stitch images check
+            validators[3](os.path.join(tmpdir, 'cb_0', RUN_DIR_NAME, 'stitched_images'))
+
+    except OSError:
+        warnings.warn('Temporary file cleanup was incomplete.')
 
     pass
