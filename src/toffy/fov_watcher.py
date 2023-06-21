513317f5513044875d96724e4d1bbdad59747899
alex-l-kong <alkong@ucdavis.edu>
2023-06-21 15:52:04 -0700

    Remove warning if split('.') encounters .def.hist or .pro.hist files

--- src/toffy/fov_watcher.py
+++ src/toffy/fov_watcher.py
@@ -80,10 +80,6 @@ class RunStructure:
 
         # filename is not corrct format of fov.bin or fov.json
         if len(filename.split(".")) != 2:
-            warnings.warn(
-                f"The file {filename} is not a valid FOV file and will be skipped from processing.",
-                Warning,
-            )
             return False, ""
 
         fov_name, extension = filename.split(".")
