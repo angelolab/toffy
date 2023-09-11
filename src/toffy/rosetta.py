23d288f318f65e50bf7f64e88c6c78e1376c23f5
alex-l-kong <alkong@ucdavis.edu>
2023-09-11 12:38:18 -0700

    Clarifying comments

--- src/toffy/rosetta.py
+++ src/toffy/rosetta.py
@@ -514,6 +514,8 @@ def create_tiled_comparison(
         if img_size_scale:
             tiled_image = rescale_images(tiled_image, img_size_scale)
         fname = os.path.join(output_dir, channels[j] + "_comparison.tiff")
+
+        # save, compress to 3 decimal points for space optimization
         image_utils.save_image(fname, np.round(tiled_image, 3))
 
 
@@ -577,7 +579,7 @@ def add_source_channel_to_tiled_image(
 
         rescaled_source = source_row / perc_ratio
 
-        # combine together and save
+        # combine together and save, compress to 3 decimal points for space optimization
         combined_tile = np.concatenate([rescaled_source, current_tile])
         save_name = tile_name.split(".tiff")[0] + "_source_" + source_channel + ".tiff"
         image_utils.save_image(os.path.join(output_dir, save_name), np.round(combined_tile, 3))
