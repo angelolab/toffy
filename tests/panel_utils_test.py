1a7aa61922a61296af9897a95a5479508deab6fa
Alex Kong <alkong@stanford.edu>
2024-11-12 12:09:46 -0800

    Add fix Noodle channel in legacy panel files defined prior to the extraction range fix PR

--- tests/panel_utils_test.py
+++ tests/panel_utils_test.py
@@ -156,10 +156,10 @@ def test_load_panel(mocker):
     mocker.patch("toffy.panel_utils.convert_panel", mock_panel_conversion)
     toffy_panel = pd.DataFrame(
         {
-            "Mass": [69, 71, 89],
-            "Target": ["Calprotectin", "Chymase", "Mast Cell Tryptase"],
-            "Start": [68.7, 70.7, 88.7],
-            "Stop": [69, 71, 89],
+            "Mass": [69, 71, 89, 117],
+            "Target": ["Calprotectin", "Chymase", "Mast Cell Tryptase", "Noodle"],
+            "Start": [68.7, 70.7, 88.7, 116.7],
+            "Stop": [69, 71, 89, 117],
         }
     )
 
@@ -176,8 +176,9 @@ def test_load_panel(mocker):
         # toffy panel
         toffy_panel.to_csv(os.path.join(temp_dir, "test_panel-toffy.csv"), index=False)
 
-        # check that previously converted panel is loaded
+        # check that previously converted panel is loaded with Noodle channel corrected
         panel = panel_utils.load_panel(os.path.join(temp_dir, "test_panel-toffy.csv"))
+        toffy_panel.loc[toffy_panel["Target"] == "Noodle", "Stop"] = 125
         assert panel.equals(toffy_panel)
 
         # check that original panel is not read in if converted already exists
